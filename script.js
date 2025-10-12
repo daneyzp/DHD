95379c4a3e8897185dc7f1eea15a4e351b7ccfd0
Daney <207961958+daneyzp@users.noreply.github.com>
2025-10-12 11:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const icons = {
   php: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
   zip: "https://upload.wikimedia.org/wikipedia/commons/1/14/Deepin_Icon_Theme_%E2%80%93_application-x-zip_%2841%29.svg",
   rar: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Deepin_Icon_Theme_%E2%80%93_application-x-rar_%2830%29.svg",
+txt: "https://upload.wikimedia.org/wikipedia/commons/2/23/Text-txt.svg",
+  xml: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Deepin_Icon_Theme_%E2%80%93_text-xml_%289%29.svg",
   default: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhyNCOzrzK3J0RskO1sKDtlFSkng5cq1Mn9vF7vjAAGMXbrOjqiEx2B6VL7prPKm_XiE_DE_89jiRg01npfB-BIoR7I9nSUqSbLvZG5DtCgs58Lx15Vzlzb_fHFGHWVrNSdNZVpWvNe3iO8dp0NPIOjAmIQtXY6sfNNh5hprqcB7hPpfmEOU3hgGrcxlP-1/s512/44386.png"
 };
 
